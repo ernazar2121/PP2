3b51f5dd6916783da97ef615920cb594f4bf8e83
Ernazar <erjanidrisov2009@gmail.com>
2019-02-10 01:43:47 +0600

    knfd

---
+++ week2/Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderName = @"C:/Users/maira/Desktop";
+            string path = Path.Combine(folderName, "ex2");
+            Directory.CreateDirectory(path);
+            string source = @"C:/Users/maira/Desktop/w2t4";
+            string[] files = Directory.GetFiles(source);
+            foreach (string file in files)
+            {
+                File.Copy(file, Path.Combine(path, Path.GetFileName(file)));
+                File.Delete(file);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
--- week2/Task4/Task4/Properties/AssemblyInfo.cs
+++ week2/Task4/Task4/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Task4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Task4")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("85f859c7-7e6b-47f9-bebe-5744a6cff4d8")]
+[assembly: Guid("746a51f9-fb1d-4740-8137-921223bf8559")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
