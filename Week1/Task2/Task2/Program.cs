ecf0a2d67143c07732042a2bd2eb37c67e99ab24
Ernazar <erjanidrisov2009@gmail.com>
2019-02-16 10:04:11 +0600

    Rt

--- Week1/Task2/Task2/Program.cs
+++ Week1/Task2/Task2/Program.cs
@@ -1,43 +1,26 @@
 ﻿
 using System;
 
-namespace Ex6
+namespace Task2
 {
     class Student
     {
         public string name;
-        public string surname;
+        public string id;
         public int yos;
-
-        public Student()
-        {
-
-            name = Console.ReadLine();
-            surname = Console.ReadLine();
-            yos = int.Parse(Console.ReadLine());
-        }
-        public Student(string name, string surname, int yos)
-        {
-            this.name = name;
-            this.surname = surname;
-            this.yos = yos;
-        }
-
-
-
-        public override string ToString()
-        {
-            return name + " " + surname + " " + yos;
-        }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Student Erna = new Student("Ernazar", "Idrissov", 4);
-            Console.WriteLine(Erna);
-            Console.WriteLine("Erna next yos:" + (Erna.yos + 1));
+                Student s = new Student();
+                s.name = Convert.ToString(Console.ReadLine());
+                s.id = Convert.ToString(Console.ReadLine());
+                s.yos = int.Parse(Console.ReadLine());
+                Console.Write(s.name + " " + s.id + " " + "year_of_study:" +" " +s.yos+" ");
+            Console.WriteLine("Next_year_of_study" + " " + (s.yos + 1));
+            Console.WriteLine();
+
             Console.ReadKey();
 
         }
